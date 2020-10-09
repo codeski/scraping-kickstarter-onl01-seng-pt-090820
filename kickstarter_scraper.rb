61d0b490849fb05fcdc1b08e0a4694273967acea
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-09 18:51:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,9 @@ def create_project_hash
   projects = {} 
   
   kickstarter.css("li.project.grid_4"). each do |project|
-    
+    projects[project] = {} 
+  end 
+  projects
 end
 
 create_project_hash
